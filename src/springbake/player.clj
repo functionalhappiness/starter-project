cc34392af85a1bc2cd2648f15d09db44109bcd33
Daniel Schruhl <danielschruhl@gmail.com>
2019-06-10 19:42:09 +0200

    wrap creating textures and add failing test

--- src/springbake/player.clj
+++ src/springbake/player.clj
@@ -2,8 +2,11 @@
   (:require [play-clj.g2d :as g2d]
             [play-clj.core :as pcore]))
 
+(defn create-texture [texture-path]
+  (g2d/texture texture-path))
+
 (defn create [texture-path]
-  ; (g2d/texture ...)
+  ; (create-texture ...)
   ; use resource to create texture
   ; add x and y coordinates to texture
   )
--- test/springbake/player_test.clj
+++ test/springbake/player_test.clj
@@ -3,6 +3,13 @@
             [springbake.player :as player]))
 
 (deftest create-test
-  (testing "should add ")
-  (is (= first second))
-  )
+  (testing "should call texture and add coordinates"
+    (with-redefs [player/create-texture (fn [texture-path]
+                                          (is (= "foo" texture-path))
+                                          {:player texture-path})]
+      (is (= {:player "foo"
+              :height 48
+              :width  48
+              :x      0
+              :y      50}
+             (player/create "foo"))))))
