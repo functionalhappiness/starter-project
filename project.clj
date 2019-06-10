952c6e7f291a5bd82c960a3d5c1e0626154398a2
Daniel Schruhl <danielschruhl@gmail.com>
2019-06-10 20:26:13 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject game "0.0.1-SNAPSHOT"
-  :description "FIXME: write description"
-  
+(defproject springbrake "0.0.1-SNAPSHOT"
+  :description "Platform jump and run game"
+
   :dependencies [[com.badlogicgames.gdx/gdx "1.8.0"]
                  [com.badlogicgames.gdx/gdx-backend-lwjgl "1.8.0"]
                  [com.badlogicgames.gdx/gdx-box2d "1.8.0"]
@@ -11,9 +11,9 @@
                   :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-platform "1.8.0"
                   :classifier "natives-desktop"]
-                 [org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojure "1.9.0"]
                  [play-clj "1.0.0"]]
-  
+
   :source-paths ["src"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [springbake.launcher]
